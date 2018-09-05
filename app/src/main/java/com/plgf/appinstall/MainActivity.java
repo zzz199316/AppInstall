031baede9e608f8aaa09a7572c5b07bdba22bcc2
PilotDeveloperAccount <1038883524@qq.com>
2018-09-05 09:20:03 +0800

    Initial commit

---
+++ app/src/main/java/com/plgf/appinstall/MainActivity.java
@@ -0,0 +1,108 @@
+package com.plgf.appinstall;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.plgf.appinstall.utils.SilentInstall;
+import java.io.File;
+
+public class MainActivity extends CheckPermissionsActivity {
+
+    TextView apkPathText;
+
+    String apkPath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        apkPathText = (TextView) findViewById(R.id.apkPathText);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            apkPath = data.getStringExtra("apk_path");
+            apkPathText.setText(apkPath);
+        }
+    }
+
+    public void onChooseApkFile(View view) {
+        Intent intent = new Intent(this, FileExplorerActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+    public void onSilentInstall(View view) {
+        if (!isRoot()) {
+            Toast.makeText(this, "没有ROOT权限，不能使用秒装", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(apkPath)) {
+            Toast.makeText(this, "请选择安装包！", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        final Button button = (Button) view;
+        button.setText("安装中");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                SilentInstall installHelper = new SilentInstall();
+                final boolean result = installHelper.install(apkPath);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (result) {
+                            Toast.makeText(MainActivity.this, "安装成功！", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "安装失败！", Toast.LENGTH_SHORT).show();
+                        }
+                        button.setText("秒装");
+                    }
+                });
+
+            }
+        }).start();
+
+    }
+
+
+
+    /**
+     * 判断手机是否拥有Root权限。
+     * @return 有root权限返回true，否则返回false。
+     */
+    public boolean isRoot() {
+        boolean bool = false;
+        try {
+            bool = new File("/system/bin/su").exists() || new File("/system/xbin/su").exists();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bool;
+    }
+
+
+    public void onForwardToAccessibility(View view) {
+        Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+        startActivity(intent);
+    }
+
+    public void onSmartInstall(View view) {
+        if (TextUtils.isEmpty(apkPath)) {
+            Toast.makeText(this, "请选择安装包！", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Uri uri = Uri.fromFile(new File(apkPath));
+        Intent localIntent = new Intent(Intent.ACTION_VIEW);
+        localIntent.setDataAndType(uri, "application/vnd.android.package-archive");
+        startActivity(localIntent);
+    }
+
+}
+
